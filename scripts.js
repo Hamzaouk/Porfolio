793ef3dcde7b5ee2d88693aa49ae6f4a7ad1905c
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2025-01-12 23:00:35 +0100

    Ajouter Active attribut dans le navbar dans le fichier js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,32 @@
 $(document).ready(function() {
-    // Smooth scroll function with faster animation
-    $('a[href^="#"]').on('click', function(event) {
-      var target = $(this.getAttribute('href'));
-      if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-          scrollTop: target.offset().top
-        }, 500); 
-      }
-    });
+  // Smooth scroll function with faster animation
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 500); 
+    }
   });
-  
-  
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  const sections = document.querySelectorAll('main > div[id]');
+
+  function setActiveLink() {
+      let index = sections.length;
+
+      while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+      
+      navLinks.forEach((link) => link.classList.remove('active'));
+      navLinks[index].classList.add('active');
+  }
+
+  setActiveLink();
+  window.addEventListener('scroll', setActiveLink);
+});
+
+
